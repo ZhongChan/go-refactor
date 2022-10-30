9197a106eaa4b04ccf6294e19b3310951e1fe6ec
SM1701 <717513736@qq.com>
2022-10-30 20:34:18 +0800

    amountFor

--- ch1/v2.go
+++ ch1/v2.go
@@ -21,22 +21,7 @@ func (V2) Statement(invoices Invoice, plays map[string]Play) string {
 		}
 		play := plays[perf.PlayID]
 
-		thisAmount := 0.0
-		switch play.Type {
-		case "tragedy":
-			thisAmount = 40000
-			if perf.Audience > 30 {
-				thisAmount += 1000 * (perf.Audience - 30)
-			}
-		case "comedy":
-			thisAmount = 30000
-			if perf.Audience > 20 {
-				thisAmount += 10000 + 500*(perf.Audience-20)
-			}
-			thisAmount += 300 * perf.Audience
-		default:
-			panic(fmt.Sprintf(`unknown type:%s`, play.Type))
-		}
+		thisAmount := amountFor(play, perf)
 
 		volumeCredits += math.Max(perf.Audience-30, 0)
 		if "comedy" == play.Type {
@@ -48,5 +33,25 @@ func (V2) Statement(invoices Invoice, plays map[string]Play) string {
 
 	result += fmt.Sprintf("Amount owed is $%.2f\n", totalAmount/100)
 	result += fmt.Sprintf("You earned %0.f credits\n", volumeCredits)
+	return result
+}
+
+func amountFor(play Play, perf struct {PlayID string `json:"playID"`;Audience float64 `json:"audience"`}) float64 {
+	result := 0.0
+	switch play.Type {
+	case "tragedy":
+		result = 40000
+		if perf.Audience > 30 {
+			result += 1000 * (perf.Audience - 30)
+		}
+	case "comedy":
+		result = 30000
+		if perf.Audience > 20 {
+			result += 10000 + 500*(perf.Audience-20)
+		}
+		result += 300 * perf.Audience
+	default:
+		panic(fmt.Sprintf(`unknown type:%s`, play.Type))
+	}
 	return result
 }
